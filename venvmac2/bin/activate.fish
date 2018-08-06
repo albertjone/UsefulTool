591b8f98d0d78481a48add27be8622217e54adc7
guanxiaojue <guan.xiaojue@99cloud.net>
2018-08-06 10:11:23 +0800

    remove venv of others

--- venvmac2/bin/activate.fish
+++ venvmac2/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/mrrobot/Desktop/UsefulTool/venv2"
+set -gx VIRTUAL_ENV "/Users/mrrobot/Desktop/UsefulTool/venvmac2"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
